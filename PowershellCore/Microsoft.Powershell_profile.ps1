576532cfa03094e85b4812569dd0022008dcfa09
Kael <kaelanreece@gmail.com>
2020-05-04 18:45:21 +1000

    update powershell profile. Add dir colors, vscode and windows terminal settings

--- PowershellCore/Microsoft.Powershell_profile.ps1
+++ PowershellCore/Microsoft.Powershell_profile.ps1
@@ -1,21 +1,88 @@
-$host.ui.RawUI.WindowTitle = "Powershell 6”
+# import modules
+Import-Module DirColors
+Import-Module posh-git
+Import-Module oh-my-posh
 
+# Dir colors settings
+Update-DirColors ~\dir_colors
+
+# oh my posh settings
+$DefaultUser = 'kaela'
+Set-Theme Paradox
+
+# ps readline settings
 Remove-PSReadlineKeyHandler -Key Tab
 Set-PSReadlineKeyHandler -Chord Tab -Function Complete
 Set-PSReadlineKeyHandler -Function DeleteCharOrExit -Chord Ctrl+D
 Set-PSReadlineOption -BellStyle None
+Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
+Set-PSReadlineOption -HistorySavePath C:\temp\history.txt
 
+# windows terminal setting to allow ctr backspace
+if ($null -ne $env:WT_SESSION ) {
+	Set-PSReadLineKeyHandler -Key Ctrl+h -Function BackwardKillWord
+}
+
+# register dotnet argument completer
+$scriptblock = {
+	param($wordToComplete, $commandAst, $cursorPosition)
+	dotnet complete --position $cursorPosition $commandAst.ToString() | ForEach-Object {
+		[System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+	}
+}
+Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock $scriptblock
+
+# Creates a folder with a particular name in all sub folders.
+function New-SubFolder {
+	Param(
+		[Parameter(Mandatory = $true)]
+		[string]$folderName
+	)
+	Get-ChildItem -Directory | ForEach-Object {
+		Push-Location $_;
+
+		# create the directory if it does not exist.
+		if (!Test-Path -Path ./$folderName) {
+			mkdir $folderName
+		}
+	}
+}
+
+# Hard reset of database using entity framework for the project
+# in the current directory.
+function Reset-DatabaseEf {
+	Remove-Item ./Migrations -Recurse -Force
+	dotnet ef database drop
+	dotnet ef migrations add Initial
+	dotnet ef database update
+}
+
+Set-Alias ex { explorer . }
+
+# navigate to a location, execute a command and navigate back.
 function cdc {
 	Param(
 		[string]$path,
 		[scriptblock]$command
 	)
-	$here = pwd;
-	cd $path;
+	$here = Get-Location;
+	Set-Location $path;
 	try {
 		& $command;
 	}
  finally {
-		cd $here;
+		Set-Location $here;
 	}
+}
+
+# copies the contents of a file into a new file, but without duplicated lines
+function Remove-Duplicates {
+	param (
+		[Parameter(Mandatory = $true)]
+		[string]
+		$Filepath
+	)
+	$FileObject = Get-ChildItem $FilePath
+	(Get-Content $FilePath | Select-Object -Unique) > "$($FileObject.Name)-NoDuplicates.txt"
 }
